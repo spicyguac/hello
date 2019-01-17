691b8c5801d610ae0e5afb210ff31e97ab0f2f77
Bruno <46731258+spicyguac@users.noreply.github.com>
2019-01-17 17:51:56 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+
+/***  Author: Bruno Garcia, Thomas Casburn
+*
+Date: 2016/11/02**
+A simple hello world program in C**/
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv) {
+  printf("Hello Bruno and Thomas\n");
+  return 0;
+}
