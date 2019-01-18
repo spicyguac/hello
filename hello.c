9cea59e392a387811b07783721dae0fc6a757cc1
Bruno <46731258+spicyguac@users.noreply.github.com>
2019-01-17 18:42:45 -0600

    mistake
    Misread directions

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ A simple hello world program in C**/
 #include<stdlib.h>
 #include<stdio.h>
 int main(int argc, char **argv) {
-  printf("Hello Bruno and Thomas\n");
+  printf("Hello Bruno");
   printf("Computer Science");
   return 0;
 }
